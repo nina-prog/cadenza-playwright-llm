ab2994ef14d8da943743d083eff3323b38ac7522
Johannes <to.thebe4t@gmail.com>
2024-06-09 11:39:22 +0200

    Add file test_case_example_Johannes_1.py with example test and screenshot from website

---
+++ src/ui_tests/test_cases/test_case_example_Johannes_1.py
@@ -0,0 +1,16 @@
+import time
+from playwright.sync_api import expect, sync_playwright
+
+
+def test_load_cadenza_website_and_find_Willkommen_bei_disy_Cadenza_text_and_make_screenshot():
+    with sync_playwright() as p:
+        browser = p.webkit.launch(headless=False)
+        page = browser.new_page()
+        page.goto("http://localhost:8080/cadenza/")
+        # Wait 3 seconds to create a screenshot from the fully loaded website.
+        time.sleep(3)
+        page.screenshot(path="temp_images/example.png")
+
+        # Expect a title "to contain" a substring.
+        expect(page.get_by_text("Willkommen bei disy Cadenza")).to_be_visible()
+        browser.close()
