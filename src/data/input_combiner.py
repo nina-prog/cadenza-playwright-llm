4a7ac3fbd244bef81dac93522b26b33998a147e0
Johannes <to.thebe4t@gmail.com>
2024-06-22 15:56:57 +0200

    Add method create_combined_input_for_multimodal_model in input_combiner.py

--- src/data/input_combiner.py
+++ src/data/input_combiner.py
@@ -18,3 +18,21 @@ def create_combined_input(html_text: str, image_text: str, precondition_text: st
                      f"Image Content:\n{image_text}\n\n" \
                      f"Playwright Test Precondition:\n{precondition_text}"
     return combined_input
+
+
+def create_combined_input_for_multimodal_model(description: str, html_text: str, precondition_text: str, prompt: str):
+    """ Combine the input data into a single string for the LLM.
+
+    :param description: The description what the model should do in general.
+    :param html_text: The text content of the HTML file.
+    :param precondition_text: The text content of the Python file.
+    :param prompt: The prompt for the LLM.
+    :return: The combined input string.
+    """
+    combined_input = f"HTML Content:\n{html_text}\n\n" \
+                     f"Image Content:\n <image>\n\n" \
+                     f"Playwright Test Precondition:\n{precondition_text}\n\n" \
+                     f"Perform action on the website: {prompt}\n\n" \
+                     f"Description:\n{description}\n\n" \
+                     f"Assistant: "
+    return combined_input
