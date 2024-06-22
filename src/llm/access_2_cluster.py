fd9d46e8faea9fcce936ba8c00ff604e2e9135da
Johannes <to.thebe4t@gmail.com>
2024-06-22 15:55:17 +0200

    Create pickle file in run_inference instead of text file and change the execution of the notebook cells in start_llm in access_2_cluster.py

--- src/llm/access_2_cluster.py
+++ src/llm/access_2_cluster.py
@@ -2,13 +2,14 @@ import time
 
 from playwright.async_api import async_playwright
 import os
+import pickle
 
 
 class Access2Cluster:
     # static magic strings
     url_to_server = 'https://jupyter-test.scc.kit.edu/user/uksco/'
     url_to_main_folder = 'https://jupyter-test.scc.kit.edu/user/uksco/lab/tree/PSDA/Uebung3'
-    path_to_input_file = 'input.txt'
+    path_to_input_file = 'input.pkl'
     target_dir_of_output_file = ''
     name_of_jupyter_notebook = ''
 
@@ -22,29 +23,27 @@ class Access2Cluster:
         self.browser = await playwright.chromium.launch(headless=False)
         self.page = await self.browser.new_page()
         await self.page.goto(self.url_to_server)
-        await self.page.get_by_role("button", name="Sign in with your KIT Account").click()
 
     async def start_llm(self):
         await self.page.goto(self.url_to_main_folder)
+        await self.page.get_by_label("File Browser Section").get_by_text("llm.ipynb", exact=True).click()
+        await self.page.get_by_text("# Install packages").click()
         await (self.page.get_by_label("llm.ipynb")
                .get_by_title("Run this cell and advance (").get_by_role("button").click())
+        await self.page.get_by_text("# Login Huggingface").click()
         await (self.page.get_by_label("llm.ipynb")
                .get_by_title("Run this cell and advance (").get_by_role("button").click())
+        await self.page.get_by_text("# Import packages").click()
         await (self.page.get_by_label("llm.ipynb")
                .get_by_title("Run this cell and advance (").get_by_role("button").click())
-        await (self.page.get_by_label("llm.ipynb")
-               .get_by_title("Run this cell and advance (").get_by_role("button").click())
+        await self.page.get_by_text("# Load model").click()
         await (self.page.get_by_label("llm.ipynb")
                .get_by_title("Run this cell and advance (").get_by_role("button").click())
 
     async def run_inference(self, input):
-        input_file = open("input.txt", "w")
-        input_file.write(str(input))
+        input_file = open("input.pkl", "wb")
+        pickle.dump(input, input_file)
         input_file.close()
-
-        await self.page.get_by_text("# Here starts the inference").click()
-        await (self.page.get_by_label("llm.ipynb")
-               .get_by_title("Run this cell and advance (").get_by_role("button").click())
 
         # This code uploads the input file
         async with self.page.expect_file_chooser() as fc_info:
@@ -52,9 +51,14 @@ class Access2Cluster:
         file_chooser = await fc_info.value
         await file_chooser.set_files(self.path_to_input_file)
 
-        # This code downloads the output file
-        # Wait, because the inference duration is over the timeout (30s)
+        await self.page.get_by_text("# Here starts the inference code of Llava").click()
+        await (self.page.get_by_label("llm.ipynb")
+               .get_by_title("Run this cell and advance (").get_by_role("button").click())
+
+        # Sleep because sometimes the predifined waiting time of Playwright (30s) is not enough.
         time.sleep(5)
+
+        # This code downloads the output file
         await self.page.get_by_text("output.txt", exact=True).click(button="right")
         async with self.page.expect_download() as download_info:
             await self.page.get_by_role("menu").get_by_text("Download", exact=True).click()
@@ -70,7 +74,7 @@ class Access2Cluster:
         output = output_file.read()
         output_file.close()
         os.remove("output.txt")
-        os.remove("input.txt")
+        os.remove("input.pkl")
         return output
 
     async def shutdown(self):
