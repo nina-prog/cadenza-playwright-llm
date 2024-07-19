e262ac301de4057fdc8af11fbf4ce11576ebcf54
Johannes <to.thebe4t@gmail.com>
2024-07-19 23:56:14 +0200

    Add finetune predictions of test set in data\prediction

---
+++ data/prediction/pred_test_script_finetuned_T5_sc+_html+_single_test_set/15_3.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+import { writeFileSync } from 'fs';
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8080/cadenza/');
+  await page.getByRole('link', { name: 'Anmelden' }).click();
+  await page.getByLabel('Benutzername *').click();
+  await page.getByLabel('Benutzername *').fill('Admin');
+  await page.getByLabel('Benutzername *').press('Tab');
+  await page.getByPlaceholder(' ').fill('Admin');
+  await page.getByRole('button', { name: 'Anmelden' }).click();
+  await page.getByText('Arbeitsmappe Testmappe_Ladesaeulen').hover();
+  await page.getByRole('link', { name: 'Testmappe_Ladesaeulen' }).click();
+  await page.getByText('Uebersicht').hover();
+  await page.getByRole('link', { name: 'Uebersicht' }).click();
+});
